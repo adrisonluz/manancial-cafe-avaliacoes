3365ceb4eb3321def53922aa1d6aa2211eac23a1
Adrison Luz <adrison701@gmail.com>
2025-09-11 14:17:35 +0000

    Altere o nome do projeto para "Manancial Café - Avaliação"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster"
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "AvaliaAí",
+  title: "Manancial Café - Avaliação",
   description: "Página de avaliação de serviços",
 };
 
